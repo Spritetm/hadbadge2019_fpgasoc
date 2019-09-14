6dcda65fc79966dd641e62626bba5270078f2dbd
Jeroen Domburg <git@j0h.nl>
2019-09-14 17:04:45 +0800

    Add protov3, add IrDA UART

---
+++ soc/ledctl.v
@@ -0,0 +1,32 @@
+module ledctl (
+	input clk,
+	input rst,
+	input [8:0] led,
+	output reg [10:0] ledc,
+	output reg [2:0] leda
+);
+
+reg [7:0] ctr;
+
+
+always @(posedge clk) begin
+	if (rst) begin
+		ctr <= 0;
+		leda <= 0;
+		ledc <= 0;
+	end else begin
+		ctr <= ctr + 1;
+		if (ctr == 0) begin
+			leda <= 'b001;
+			ledc <= { 2'b11, (led & 9'b100010101)};
+		end else if (ctr == 85) begin
+			leda <= 'b010;
+			ledc <= { 2'b00, (led & 9'b000101010)};
+		end else if (ctr == 170) begin
+			leda <= 'b100;
+			ledc <= { 2'b00, (led & 9'b011000000)};
+		end
+	end
+end
+
+endmodule
