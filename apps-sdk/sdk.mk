dca229f17e68dbab57496b1d8671f11abbdc24c7
Jeroen Domburg <git@j0h.nl>
2019-10-09 10:34:36 +0800

    Fix compiler flags so it always picks the correct arch

--- apps-sdk/sdk.mk
+++ apps-sdk/sdk.mk
@@ -29,8 +29,8 @@ BUILD_DIR_SDK := $(BUILD_DIR)/apps-sdk
 
 #Ipl gloss is in include path because mach_defines.h
 INCLUDEDIRS += $(APPSSDK_DIR) $(APPSSDK_DIR)/gloss $(APPSSDK_DIR)/../soc/ipl/gloss $(APPSSDK_DIR)/../soc/ipl/syscallable/
-CFLAGS += -Os -ggdb $(addprefix -I,$(INCLUDEDIRS))
-LDFLAGS += -Wl,-Bstatic -Wl,--gc-sections -Wl,-T,$(LDSCRIPT) -Wl,-Map,$(TARGET_MAP) -lgcc -lm -nostartfiles
+CFLAGS += -Os -ggdb $(addprefix -I,$(INCLUDEDIRS)) -march=rv32im -mabi=ilp32
+LDFLAGS += -Wl,-Bstatic -Wl,--gc-sections -Wl,-T,$(LDSCRIPT) -Wl,-Map,$(TARGET_MAP) -lgcc -lm -nostartfiles -Wl,-melf32lriscv
 DEPFLAGS := -MMD -MP 
 
 export PREFIX CC AR LD OBJCOPY CFLAGS LDFLAGS APPNAME
