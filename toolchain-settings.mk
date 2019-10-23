17eb2c12dcb1b3e21a00a4e23349233a7c055210
Sylvain Munaut <tnt@246tNt.com>
2019-10-23 17:37:20 +0200

    build: Consolidate toolchain config in one file and add auto-detect
     - All settings wrt to the RISC-V toolchain are in a single file
     - All flags related to the architecture (ABI/Arch) are in there as well
     - It tries to auto-detect the available toolchain
     - It uses the standard CROSS variable rather than PREFIX which usually
       means 'install prefix' in Makefiles
    
    Signed-off-by: Sylvain Munaut <tnt@246tNt.com>

---
+++ toolchain-settings.mk
@@ -0,0 +1,32 @@
+#
+# Setups the RISC-V toolchain
+#
+
+# Find a toolchain
+POSSIBLE_CROSS = riscv-none-embed- riscv32-unknown-elf- riscv64-unknown-elf-
+
+ifneq ("$(RISCV_TOOLCHAIN_PATH)", "")
+POSSIBLE_CROSS := $(addprefix $(RISCV_TOOLCHAIN_PATH), $(POSSIBLE_CROSS))
+endif
+
+ifeq ("$(CROSS)", "")
+CROSS := $(shell for c in $(POSSIBLE_CROSS); do which $${c}gcc >/dev/null 2>&1 && echo $${c} && break; done)
+endif
+
+ifeq ("$(CROSS)", "")
+$(error Need a RISC-V toolchain)
+endif
+
+# Set prefixes and paths to all tools.
+CC := $(CROSS)gcc
+AR := $(CROSS)ar
+LD := $(CROSS)ld
+OBJCOPY := $(CROSS)objcopy
+OBJDUMP := $(CROSS)objdump
+SIZE := $(CROSS)size
+STRIP := $(CROSS)strip
+GDB := $(CROSS)gdb
+
+ASFLAGS := -march=rv32im -mabi=ilp32
+CFLAGS  := -march=rv32im -mabi=ilp32 -Os
+LDFLAGS := -march=rv32im -mabi=ilp32 -ffreestanding -nostartfiles -Wl,--gc-section -Wl,-Bstatic -Wl,-melf32lriscv 
