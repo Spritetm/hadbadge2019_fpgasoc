ce2147744b1d01323ad0fe37e9be1557220fbb20
Zapp <zapp@andnxor.com>
2019-10-24 20:33:55 -0700

    Finally some graphics

--- app-flappy/main.c
+++ app-flappy/main.c
@@ -1,6 +1,7 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "mach_defines.h"
 #include "sdk.h"
@@ -11,8 +12,8 @@
 //and one past the last byte of it.
 extern char _binary_bgnd_png_start;
 extern char _binary_bgnd_png_end;
-extern char _binary_tileset_default_png_start;
-extern char _binary_tileset_default_png_end;
+extern char _binary_flappy_tileset_png_start;
+extern char _binary_flappy_tileset_png_end;
 
 //Pointer to the framebuffer memory.
 uint8_t *fbmem;
@@ -31,6 +32,11 @@ extern uint32_t GFXTILES[];
 extern uint32_t GFXTILEMAPA[];
 extern uint32_t GFXTILEMAPB[];
 
+//Define some tilemap data
+#define FLAPPY_GROUND_INDEX 237
+#define FLAPPY_GROUND_Y 19
+#define FLAPPY_BRICK_INDEX 265
+
 uint32_t __score = 0;
 
 //Borrowed this from lcd.c until a better solution comes along :/
@@ -38,6 +44,11 @@ static void __INEFFICIENT_delay(int n) {
 	for (int i=0; i<n; i++) {
 		for (volatile int t=0; t<(1<<11); t++);
 	}
+}
+
+//Helper function to set a tile on layer a
+static void __tile_a_set(uint8_t x, uint8_t y, uint32_t index) {
+	GFXTILEMAPA[y*GFX_TILEMAP_W+x] = index;
 }
 
 void main(int argc, char **argv) {
@@ -60,7 +71,7 @@ void main(int argc, char **argv) {
 	//Load up the default tileset and font.
 	//ToDo: loading pngs takes a long time... move over to pcx instead.
 	printf("Loading tiles...\n");
-	gfx_load_tiles_mem(GFXTILES, &GFXPAL[0], &_binary_tileset_default_png_start, (&_binary_tileset_default_png_end-&_binary_tileset_default_png_start));
+	int gfx_tiles_err = gfx_load_tiles_mem(GFXTILES, &GFXPAL[0], &_binary_flappy_tileset_png_start, (&_binary_flappy_tileset_png_end-&_binary_flappy_tileset_png_start));
 	printf("Tiles initialized\n");
 
 	
@@ -90,22 +101,55 @@ void main(int argc, char **argv) {
 	GFXPAL[FB_PAL_OFFSET+0x1ff]=0x40ff00ff; //so it becomes this instead.
 
 	//Wait until all buttons are released
-	while (MISC_REG(MISC_BTN_REG)) ;
+	while (MISC_REG(MISC_BTN_REG));
 
 	fprintf(console, "\0330M\033C\0330A"); //Set map to tilemap A, clear tilemap, set attr to 0
 	fprintf(console, "\033C"); //clear the console. Note '\033' is the escape character.
 	
-	fprintf(console, "\03324X"); //set Xpos to 5
-	fprintf(console, "\0330Y"); //set Ypos to 8
-	fprintf(console, "Flappy\n"); // Print a nice greeting.
+	fprintf(console, "\03324X\0330YFlappy\n"); 
+	fprintf(console, "\03324X\03310YERR: %d\n", gfx_tiles_err);
 	// //Note that without the newline at the end, all printf's would stay in the buffer.
 
 	// cache_flush(fbmem, fbmem+FB_WIDTH*FB_HEIGHT);
-	
+
+	uint8_t i=0;
+	for (uint8_t x=0; x<30; x++) {
+		for (uint8_t y=0;y<9; y++) {
+			GFXTILEMAPA[y*GFX_TILEMAP_W+x] = i++;
+		}
+	}
+
+	//Wait for button press then start game
+	// while((MISC_REG(MISC_BTN_REG) & BUTTON_A)==0);
+	// //Wait until all buttons are released
+	// while (MISC_REG(MISC_BTN_REG));
+	memset(GFXTILEMAPA,0,0x4000); //Clear tilemap a
+	memset(GFXTILEMAPB,0,0x4000); //Clear tilemap b
+
+	//Draw the ground on the tilemap, probably inefficient but we're learning here
+	for (uint8_t x=0; x<30; x++) {
+		__tile_a_set(x, FLAPPY_GROUND_Y, FLAPPY_GROUND_INDEX);
+	}
+
+	//Draw a bottom "pipe"
+	__tile_a_set(20,16,FLAPPY_BRICK_INDEX);
+	__tile_a_set(21,16,FLAPPY_BRICK_INDEX+1);
+	__tile_a_set(22,16,FLAPPY_BRICK_INDEX+1);
+	__tile_a_set(23,16,FLAPPY_BRICK_INDEX+2);
+	__tile_a_set(20,17,FLAPPY_BRICK_INDEX);
+	__tile_a_set(21,17,FLAPPY_BRICK_INDEX+1);
+	__tile_a_set(22,17,FLAPPY_BRICK_INDEX+1);
+	__tile_a_set(23,17,FLAPPY_BRICK_INDEX+2);
+	__tile_a_set(20,18,FLAPPY_BRICK_INDEX);
+	__tile_a_set(21,18,FLAPPY_BRICK_INDEX+1);
+	__tile_a_set(22,18,FLAPPY_BRICK_INDEX+1);
+	__tile_a_set(23,18,FLAPPY_BRICK_INDEX+2);
+
 	//The user can still see nothing of this graphics goodness, so let's re-enable the framebuffer and
 	//tile layer A (the default layer for the console). 
 	//Normal FB enabled (vice 8 bit) because background is loaded into the framebuffer above in 4 bit mode. 
-	 GFX_REG(GFX_LAYEREN_REG)=GFX_LAYEREN_FB|GFX_LAYEREN_FB|GFX_LAYEREN_TILEA;
+	//TILEA is where text is printed by default
+	 GFX_REG(GFX_LAYEREN_REG)=GFX_LAYEREN_FB|GFX_LAYEREN_TILEA|GFX_LAYEREN_TILEB;
 
 	//Primary game loop
 	 while((MISC_REG(MISC_BTN_REG) & BUTTON_A)==0) {
