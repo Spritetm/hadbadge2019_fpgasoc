cefafdd8901ccec3be7977475b0c402fd24fc231
Roger Cheng <roger.random@outlook.com>
2019-11-05 19:58:48 -0800

    Incorporate SEGA splash into autoexec

--- app-autoexec/main.c
+++ app-autoexec/main.c
@@ -24,6 +24,9 @@ extern char _binary_ps_bg_png_end;
 extern char _binary_ps_tiles_png_start;
 extern char _binary_ps_tiles_png_end;
 
+extern char _binary_sega_tileset_png_start;
+extern char _binary_sega_tileset_png_end;
+
 extern char _binary_switch_bg_png_start;
 extern char _binary_switch_bg_png_end;
 
@@ -77,6 +80,18 @@ uint32_t *GFXSPRITES = (uint32_t *)0x5000C000;
 
 #define TILE_PS_WAVE1    0x0
 #define TILE_PS_WAVE2    0x30
+
+/////////////////////////////////////////////////////////////////////////////
+//
+//  Constants for splash screen inspired by SEGA
+
+#define SEGA_VERTICAL_POS 	8
+#define SEGA_HORIZONTAL_POS 3
+#define TILE_SEGA_ROW1 		0x00
+#define TILE_SEGA_ROW2 		TILE_SEGA_ROW1+0x40
+#define TILE_WHITE 			TILE_SEGA_ROW1+0x38 // Reusing a tile from "P" in "SUPERCON" that happens to be all white.
+#define TILE_SEGA_CYAN 		TILE_SEGA_ROW2+0x08
+#define PAL_SEGA_CYAN 		0
 
 /////////////////////////////////////////////////////////////////////////////
 //
@@ -397,6 +412,180 @@ void playstation_splash() {
 
 /////////////////////////////////////////////////////////////////////////////
 //
+//  Called by main() to run splash screen inspired by SEGA. This
+//  function is fully self-contained and can be main() for a standaline app.
+//  Be sure to copy the relevant constant #define above if doing so.
+
+void sega_splash() {
+	//Allocate framebuffer memory
+	fbmem=malloc(320*512/2);
+
+	// Turn off top row LEDs
+	MISC_REG(MISC_LED_REG)=0;
+
+	//Set up the framebuffer address.
+	GFX_REG(GFX_FBADDR_REG)=((uint32_t)fbmem)&0xFFFFFF;
+	//We're going to use a pitch of 512 pixels, and the fb palette will start at 256.
+	GFX_REG(GFX_FBPITCH_REG)=(FB_PAL_OFFSET<<GFX_FBPITCH_PAL_OFF)|(512<<GFX_FBPITCH_PITCH_OFF);
+	//Blank out fb while we're loading stuff.
+	GFX_REG(GFX_LAYEREN_REG)=0;
+
+	//Load up the default tileset and font.
+	//ToDo: loading pngs takes a long time... move over to pcx instead.
+	printf("Loading tiles...\n");
+	int gfx_tiles_err = gfx_load_tiles_mem(GFXTILES, &GFXPAL[0], &_binary_sega_tileset_png_start, (&_binary_sega_tileset_png_end-&_binary_sega_tileset_png_start));
+	printf("Tiles initialized err=%d\n", gfx_tiles_err);
+
+
+	//The IPL leaves us with a tileset that has tile 0 to 127 map to ASCII characters, so we do not need to
+	//load anything specific for this. In order to get some text out, we can use the /dev/console device
+	//that will use these tiles to put text in a tilemap. It uses escape codes to do so, see
+	//ipl/gloss/console_out.c for more info.
+	//Note that without the setvbuf command, no characters would be printed until 1024 characters are
+	//buffered.
+	console=fopen("/dev/console", "w");
+	setvbuf(console, NULL, _IOLBF, 1024); //make console line buffered
+	if (console==NULL) {
+		printf("Error opening console!\n");
+	}
+
+	//Now, use a library function to load the image into the framebuffer memory. This function will also set up the palette entries,
+	//we tell it to start writing from entry 0.
+	//PAL offset changes the colors that the 16-bit png maps to?
+	//gfx_load_fb_mem(fbmem, &GFXPAL[FB_PAL_OFFSET], 4, 512, &_binary_badgetris_bgnd_png_start, (&_binary_badgetris_bgnd_png_end-&_binary_badgetris_bgnd_png_start));
+
+	//Flush the memory region to psram so the GFX hw can stream it from there.
+	cache_flush(fbmem, fbmem+FB_WIDTH*FB_HEIGHT);
+
+	//Copied from IPL not sure why yet
+	GFXPAL[FB_PAL_OFFSET+0x100]=0x00ff00ff; //Note: For some reason, the sprites use this as default bgnd. ToDo: fix this...
+	GFXPAL[FB_PAL_OFFSET+0x1ff]=0x40ff00ff; //so it becomes this instead.
+
+	//This makes sure not to read button still pressed from badge menu selection
+	__button_wait_for_release();
+
+	//Set map to tilemap B, clear tilemap, set attr to 0
+	//Not sure yet what attr does, but tilemap be is important as it will have the effect of layering
+	//on top of our scrolling game
+	fprintf(console, "\0331M\033C\0330A\n");
+	//Note that without the newline at the end, all printf's would stay in the buffer.
+
+
+	//Clear both tilemaps
+	memset(GFXTILEMAPA,0,0x4000);
+	memset(GFXTILEMAPB,0,0x4000);
+	//Clear sprites that IPL may have loaded
+	memset(GFXSPRITES,0,0x4000);
+
+	/********************************************************************************
+	 * Put your user code in there, return when it's time to exit back to bage menu *
+	 * *****************************************************************************/
+	uint8_t x_offset = 0;
+	uint8_t y_offset = 0;
+	uint8_t tile_index = 0;
+
+	// Fill top tilelayer B with opaque white tiles, some will be overwritten soon.
+	for (uint8_t x=0; x<TILEGRID_WIDTH; x++) {
+		for (uint8_t y=0; y<TILEGRID_HEIGHT; y++) {
+			__tile_b_set(x,y,TILE_WHITE);
+		}
+	}
+
+	// Top tilelayer B transparent "SUPERCON" replace some of the opaque white tiles
+	for (uint8_t y=0; y<4; y++) {
+		y_offset = SEGA_VERTICAL_POS+y;
+		// A full row of tiles in the tileset has "SUPER" and a bit of "C"
+		//  spread across 16 tiles wide and 4 tiles high.
+		for (uint8_t x=0; x<0x10; x++) {
+			x_offset = SEGA_HORIZONTAL_POS+x;
+			tile_index = TILE_SEGA_ROW1+x+0x10*y;
+			__tile_b_set(x_offset, y_offset, tile_index);
+		}
+
+		// Following half row (8 tiles wide, 4 high) has the rest of "C" and "ON"
+		for (uint8_t x=0; x<0x8; x++) {
+			x_offset = SEGA_HORIZONTAL_POS+0x10+x;
+			tile_index = TILE_SEGA_ROW2+x+0x10*y;
+			__tile_b_set(x_offset, y_offset, tile_index);
+		}
+	}
+
+	// Draw animated tilelayer A with items to show through transparent parts
+
+	// Top 4 rows to white, center will receive cyan bar shortly.
+	for (uint8_t x=0; x<64; x++) {
+		for (uint8_t y=0; y<4; y++) {
+			__tile_a_set(x,y,TILE_WHITE);
+		}
+	}
+
+	// Vertical cyan bar in middle of top two (now white) rows
+	for (uint8_t y=0; y<4; y++) {
+		__tile_a_set(31,y,TILE_SEGA_CYAN);
+		__tile_a_set(32,y,TILE_SEGA_CYAN);
+	}
+
+	// After vertical cyan bar sweeps, use this horizontal cyan bar to fade in
+	// blue via palette animation.
+	for (uint8_t x=0; x<64; x++) {
+		// Rows 4-7 to 25% blue
+		for (uint8_t y=4; y<8; y++) {
+			__tile_a_set(x,y,TILE_SEGA_CYAN);
+		}
+	}
+
+	// Move tile layer A in place for horizontal sweep of vertical cyan bar
+	int16_t x_translate= 64 * 16 * TILEGRID_WIDTH;
+	int16_t y_translate=-64 * 16 * SEGA_VERTICAL_POS;
+	__tile_a_translate(x_translate,y_translate);
+
+	// Tiles are set up, we can now enable layers
+	GFX_REG(GFX_LAYEREN_REG)=GFX_LAYEREN_TILEA|GFX_LAYEREN_TILEB;
+
+	// Wait a bit before starting the sweep
+	__INEFFICIENT_delay(250);
+
+	int16_t step_size = 64;
+	// Cyan bar sweeps right
+	for (uint16_t count=0; count<(64*16*TILEGRID_WIDTH); count+=step_size) {
+		__tile_a_translate(x_translate-count,y_translate);
+		__INEFFICIENT_delay(1);
+	}
+
+	// Palette color appears to be in AABBGGRR format:
+	//	0xFF0000FF Red
+	//	0xFF00FF00 Green
+	//	0xFF00FFFF Yellow
+	//	0xFFFF0000 Blue
+	//	0xFFFF00FF Magenta
+	//	0xFFFFFF00 Cyan
+	//	0xFFFFFFFF White
+
+	// Modify palette so cyan is now white
+	GFXPAL[PAL_SEGA_CYAN] = 0xFFFFFFFF;
+
+	// Wait to start fading in blue
+	__INEFFICIENT_delay(100);
+
+	// Move in the solid cyan (now palette changed to white) horizontal bar for palette animation
+	__tile_a_translate(x_translate,y_translate+64*16*4);
+
+	// Palette animation from 0xFFFFFFFF to 0xFFFF0000
+	uint32_t color = 0xFFFF0000;
+	for (uint8_t fade=0xFF; fade > 0; fade--) {
+		color = 0xFFFF0000;
+		color |= fade;
+		color |= fade << 8;
+		GFXPAL[PAL_SEGA_CYAN] = color;
+		__INEFFICIENT_delay(1);
+	}
+
+	// Logo complete, allow admiration for a short time before exiting.
+	__INEFFICIENT_delay(750);
+}
+
+/////////////////////////////////////////////////////////////////////////////
+//
 //  Called by main() to run splash screen inspired by Nintendo Switch. This
 //  function is fully self-contained and can be main() for a standaline app.
 //  Be sure to copy the relevant constant #define above if doing so.
@@ -673,6 +862,8 @@ void main(int argc, char **argv) {
 			switch_splash();
 			break;
 		case 3:
+			sega_splash();
+			break;
 		case 4:
 			playstation_splash();
 			break;
--- app-sega/README.md
+++
@@ -1,9 +0,0 @@
-# Splash screen candidate: SEGA
-
-A candidate badge splash screen, a tribute to SEGA opening screen.
-
-Reference original: https://www.youtube.com/watch?v=pWwv4zhxXMY
-
-Text via https://fontmeme.com/sega-font/
-
-YouTube video: https://youtu.be/_W5g7k2BmI4
