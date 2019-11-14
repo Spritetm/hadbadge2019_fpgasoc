a0b1a4524e137633b8ae9bd8eb443460db8d958c
Jeroen Domburg <git@j0h.nl>
2019-11-15 01:33:18 +0800

    cache.h: pragma once

--- soc/ipl/syscallable/cache.h
+++ soc/ipl/syscallable/cache.h
@@ -1,6 +1,8 @@
 #include "mach_defines.h"
 #include <stdint.h>
 
+#pragma once
+
 //Flush a certain memory region to psram
 inline static void cache_flush(void *addr_start, void *addr_end) {
 	volatile uint32_t *p = (volatile uint32_t*)(((uint32_t)addr_start & ~3) - MACH_RAM_START + MACH_FLUSH_REGION);
