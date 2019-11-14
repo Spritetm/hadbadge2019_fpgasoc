50f9a5b74ec4ca94c23d1d1bfd5911637923e084
Elliot Williams <elliot@pre.postero.us>
2019-11-14 17:33:12 +0100

    added splash screen to audio

---
+++ app-audiodemo/graphics.h
@@ -0,0 +1,16 @@
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "mach_defines.h"
+#include "gfx_load.h"
+#include "cache.h"
+
+// This is just to get the graphics out of the sound code.
+// Makes it readable.
+//
+void do_graphics(void);
+void button_wait(void);
+
+
