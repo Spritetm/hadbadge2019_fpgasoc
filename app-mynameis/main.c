7c6b7f9632c96c894c363e8e3c501b0a4cbe7011
Thomas Flummer <tf@flummer.net>
2019-11-15 13:51:23 -0800

    added “hello my name is” app
    Simple app to show you name/twitter handle/etc.
    
    Based on the hellow world app.

---
+++ app-mynameis/main.c
@@ -0,0 +1,104 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+#include "mach_defines.h"
+#include "sdk.h"
+#include "gfx_load.h"
+#include "cache.h"
+#include "badgetime.h"
+
+//The bgnd.png image got linked into the binary of this app, and these two chars are the first
+//and one past the last byte of it.
+extern char _binary_bgnd_png_start;
+extern char _binary_bgnd_png_end;
+
+//Pointer to the framebuffer memory.
+uint8_t *fbmem;
+
+#define FB_WIDTH 512
+#define FB_HEIGHT 320
+
+void gfx_set_xlate_val(int layer, int xcenter, int ycenter, float scale, float rot) {
+	float scale_inv=(1.0/scale);
+	float dx_x=cos(rot)*scale_inv;
+	float dx_y=-sin(rot)*scale_inv;
+	float dy_x=sin(rot)*scale_inv;
+	float dy_y=cos(rot)*scale_inv;
+	float start_x=-xcenter;
+	float start_y=-ycenter;
+
+	int i_dx_x=64.0*dx_x;
+	int i_dx_y=64.0*dx_y;
+	int i_dy_x=64.0*dy_x;
+	int i_dy_y=64.0*dy_y;
+	int i_start_x=(-start_x+start_x*dx_x-start_y*dx_y)*64.0;
+	int i_start_y=(-start_y+start_y*dy_y-start_x*dy_x)*64.0;
+
+	GFX_REG(GFX_TILEA_OFF)=(i_start_y<<16)+(i_start_x&0xffff);
+	GFX_REG(GFX_TILEA_INC_COL)=(i_dx_y<<16)+(i_dx_x&0xffff);
+	GFX_REG(GFX_TILEA_INC_ROW)=(i_dy_y<<16)+(i_dy_x&0xffff);
+}
+
+void main(int argc, char **argv) {
+	//We're running in app context. We have full control over the badge and can do with the hardware what we want. As
+	//soon as main() returns, however, we will go back to the IPL.
+	printf("Hello World app: main running\n");
+
+	//Blank out fb while we're loading stuff by disabling all layers. This just shows the background color.
+	GFX_REG(GFX_BGNDCOL_REG)=0x202020; //a soft gray
+	GFX_REG(GFX_LAYEREN_REG)=0; //disable all gfx layers
+	
+	//First, allocate some memory for the background framebuffer. We're gonna dump a fancy image into it. The image is
+	//going to be 8-bit, so we allocate 1 byte per pixel.
+	fbmem=calloc(FB_WIDTH,FB_HEIGHT);
+	printf("Hello World: framebuffer at %p\n", fbmem);
+	
+	//Tell the GFX hardware to use this, and its pitch. We also tell the GFX hardware to use palette entries starting
+	//from 128 for the frame buffer; the tiles left by the IPL will use palette entries 0-16 already.
+	GFX_REG(GFX_FBPITCH_REG)=(128<<GFX_FBPITCH_PAL_OFF)|(FB_WIDTH<<GFX_FBPITCH_PITCH_OFF);
+	//Set up the framebuffer address
+	GFX_REG(GFX_FBADDR_REG)=((uint32_t)fbmem);
+
+	//Now, use a library function to load the image into the framebuffer memory. This function will also set up the palette entries,
+	//we tell it to start writing from entry 128.
+	int png_size=(&_binary_bgnd_png_end-&_binary_bgnd_png_start);
+	int i=gfx_load_fb_mem(fbmem, &GFXPAL[128], 8, FB_WIDTH, &_binary_bgnd_png_start, png_size);
+	if (i) printf("gfx_load_fb_mem: error %d\n", i);
+
+	//Flush the memory region to psram so the GFX hw can stream it from there.
+	cache_flush(fbmem, fbmem+FB_WIDTH*FB_HEIGHT);
+
+	//The IPL leaves us with a tileset that has tile 0 to 127 map to ASCII characters, so we do not need to
+	//load anything specific for this. In order to get some text out, we can use the /dev/console device
+	//that will use these tiles to put text in a tilemap. It uses escape codes to do so, see 
+	//ipl/gloss/console_out.c for more info.
+	FILE *f;
+	f=fopen("/dev/console", "w");
+	setvbuf(f, NULL, _IONBF, 0); //make console line unbuffered
+	//Note that without the setvbuf command, no characters would be printed until 1024 characters are
+	//buffered. You normally don't want this.
+	fprintf(f, "\033C"); //clear the console. Note '\033' is the escape character.
+
+	fprintf(f, "\0338X"); //set Xpos to 5
+	fprintf(f, "\03312Y"); //set Ypos to 8
+	// change the line below for your own name/twitter handle, etc.
+	fprintf(f, "@thomasflummer"); // Print a nice greeting.
+	
+	// maybe change the scaling factor (1.8) below if your name is too long or very short
+	gfx_set_xlate_val(0, 240, 200, 1.8, 0);
+
+	//The user can still see nothing of this graphics goodness, so let's re-enable the framebuffer and
+	//tile layer A (the default layer for the console). Also indicate the framebuffer we have is
+	//8-bit.
+	GFX_REG(GFX_LAYEREN_REG)=GFX_LAYEREN_FB_8BIT|GFX_LAYEREN_FB|GFX_LAYEREN_TILEA;
+
+	printf("Hello World ready. Press a button to exit.\n");
+	//Wait until all buttons are released
+	wait_for_button_release();
+
+	//Wait until button A is pressed
+	wait_for_button_press(BUTTON_A);
+	printf("Hello World done. Bye!\n");
+}
